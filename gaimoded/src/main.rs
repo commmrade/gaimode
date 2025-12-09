e717d1b9502b51d2e031eda312c59c0c8ebee67f
klewy <cigankov807@gmail.com>
2025-12-09 14:35:02 +0300

    Move signal handling to gaemon

--- gaimoded/src/main.rs
+++ gaimoded/src/main.rs
@@ -12,6 +12,32 @@ fn print_info(name: &str) {
     );
 }
 
+extern "C" fn handle_sig(sig: std::ffi::c_int, act: *const libc::siginfo_t, p: *mut libc::c_void) {}
+
+fn setup_signals() {
+    unsafe {
+        let mut act: libc::sigaction = std::mem::zeroed();
+        act.sa_sigaction = handle_sig as usize;
+        // act.sa_flags = libc::SA_RESTART; // restart a syscall if it was interrupted by a signal (like waitpid)
+        act.sa_sigaction = libc::SIG_IGN; // Ignore siginфt
+
+        /*
+        * A child created via fork(2) inherits a copy of its parent's signal
+               dispositions.  During an execve(2), the dispositions of handled
+               signals are reset to the default; the dispositions of ignored
+               signals are left unchanged
+        */
+
+        libc::sigaction(
+            libc::SIGINT,
+            &act as *const libc::sigaction,
+            std::ptr::null_mut(),
+        );
+    }
+
+    // todo!("Find a library to handle this shit (do i need to handle signals");
+}
+
 fn main() {
     println!("Start");
 
