ed892f65bd1e9400ca24ac1dd6f597f863374df4
klewy <cigankov807@gmail.com>
2025-12-09 12:27:06 +0300

    Basic gaimode setup

--- gaimode/src/main.rs
+++ gaimode/src/main.rs
@@ -1,3 +1,46 @@
+use std::{
+    ffi::{CStr, CString},
+    str::FromStr,
+};
+
+use nix::{sys::wait::waitpid, unistd};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().into_iter().collect();
+    if args.len() < 2 {
+        eprintln!("You should supply path as second argument: gaimode [application] [flags]");
+        return;
+    }
+    let bin_name = args[1].clone();
+
+    match unsafe { unistd::fork() } {
+        Ok(unistd::ForkResult::Parent { child }) => {
+            // TODO: Optimization here on child PID.
+            if let Err(why) = waitpid(child, None) {
+                eprintln!("Failed to wait for child: {}", why);
+            }
+
+            println!("Process has been finished");
+        }
+        Ok(unistd::ForkResult::Child) => {
+            let mut bin_name = bin_name.into_bytes();
+            bin_name.push(0);
+
+            let mut bin_args = Vec::<CString>::new();
+            for arg in &args[1..] {
+                bin_args.push(CString::from_str(&arg).expect("Failed to create CString"));
+            }
+
+            let bin_name = CStr::from_bytes_with_nul(&bin_name).expect("Failed to create CStr");
+            let err = unistd::execvp(bin_name, &bin_args).err().unwrap(); // Safe: because returns only when there is an error
+            println!(
+                "Could not start {} because {}",
+                bin_name.to_string_lossy(),
+                err
+            );
+        }
+        Err(why) => {
+            eprintln!("Failed to fork: {}", why);
+        }
+    }
 }
--- gaimoded/src/main.rs
+++ gaimoded/src/main.rs
@@ -1,3 +1,51 @@
+use std::time::Duration;
+
+use nix::{sys::wait::waitpid, unistd};
+
+fn print_info(name: &str) {
+    println!(
+        "{} id: {}, gid: {}, sid: {}",
+        name,
+        unistd::getpid(),
+        unistd::getpgid(None).unwrap(),
+        unistd::getsid(None).unwrap(),
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Start");
+
+    match unsafe { unistd::fork() } {
+        Ok(unistd::ForkResult::Parent { child }) => {
+            print_info("parent");
+
+            if let Err(why) = waitpid(child, None) {
+                eprintln!("Failed to waitpid: {}", why);
+            }
+        }
+        Ok(unistd::ForkResult::Child) => {
+            if let Err(why) = unistd::setsid() {
+                eprintln!("setsid failed: {}", why);
+            }
+
+            print_info("Child after SID");
+
+            match unsafe { unistd::fork() } {
+                Ok(unistd::ForkResult::Child) => {
+                    print_info("Child daemon");
+
+                    loop {
+                        std::thread::sleep(Duration::from_secs(1));
+                    }
+                }
+                Ok(unistd::ForkResult::Parent { .. }) => {}
+                Err(why) => {
+                    eprintln!("Fork failed: {}", why);
+                }
+            }
+        }
+        Err(why) => {
+            eprintln!("Fork failed: {}", why);
+        }
+    }
 }
