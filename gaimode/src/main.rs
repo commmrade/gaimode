48b0e0b7f29d6278b50f742898c78dc2aca35c66
klewy <cigankov807@gmail.com>
2025-12-13 20:50:49 +0300

    Command line interface

--- gaimode/src/main.rs
+++ gaimode/src/main.rs
@@ -1,46 +1,39 @@
+mod uds;
 use std::{
     ffi::{CStr, CString},
-    io::{Read, Write},
-    net::TcpStream,
+    io::Write,
     str::FromStr,
-    time::Duration,
 };
 
+use clap::{Parser, Subcommand};
 use gaiproto::Gaiproto;
 use nix::{sys::wait::waitpid, unistd};
-mod uds;
-
-fn test_main() {
-    let mut stream = TcpStream::connect("127.0.0.1:8088").unwrap();
-    loop {
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        input = input.trim().to_owned();
-
-        let packet = Gaiproto::new(
-            (gaiproto::MIN_PACKET_SIZE + input.len()) as u32,
-            gaiproto::K_RESET_ALL,
-            input.into_bytes(),
-        );
-        let bytes = packet.convert_to_bytes();
-        stream.write_all(&bytes).unwrap();
-    }
-}
-
 const UDS_FILENAME: &'static str = "gaimoded_sock";
 
-fn main() {
-    let args: Vec<String> = std::env::args().into_iter().collect();
-    if args.len() < 2 {
-        eprintln!("You should supply path as second argument: gaimode [application] [flags]");
-        return;
-    }
-    let bin_name = args[1].clone();
+#[derive(Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    let mut path = std::env::temp_dir();
-    path.push(UDS_FILENAME);
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Run {
+        #[arg(value_name = "Binary path")]
+        executable: String,
+        #[arg(value_name = "Arguments for binary")]
+        args: Vec<String>,
+    },
+    #[command(arg_required_else_help = true)]
+    ResetProcess {
+        #[arg(value_name = "Process ID")]
+        pid: i32,
+    },
+    ResetAll,
+}
 
-    let mut stream = std::os::unix::net::UnixStream::connect(&path).unwrap();
+fn run(bin_name: String, args: Vec<String>, mut stream: std::os::unix::net::UnixStream) {
     // TODO: Spin up a daemon if socket not found
 
     match unsafe { unistd::fork() } {
@@ -58,13 +51,14 @@ fn main() {
             }
         }
         Ok(unistd::ForkResult::Child) => {
-            let mut bin_name = bin_name.into_bytes();
-            bin_name.push(0);
-
             let mut bin_args = Vec::<CString>::new();
-            for arg in &args[1..] {
+            bin_args.push(CString::from_str(&bin_name).unwrap());
+            for arg in args {
                 bin_args.push(CString::from_str(&arg).expect("Failed to create CString"));
             }
+
+            let mut bin_name = bin_name.into_bytes();
+            bin_name.push(0);
 
             let bin_name = CStr::from_bytes_with_nul(&bin_name).expect("Failed to create CStr");
             let err = unistd::execvp(bin_name, &bin_args).err().unwrap(); // Safe: because returns only when there is an error
@@ -76,6 +70,47 @@ fn main() {
         }
         Err(why) => {
             eprintln!("Failed to fork: {}", why);
+        }
+    }
+}
+
+fn reset_process(pid: i32, mut stream: std::os::unix::net::UnixStream) {
+    let packet = Gaiproto::new(
+        (gaiproto::MIN_PACKET_SIZE + std::mem::size_of_val(&pid)) as u32,
+        gaiproto::K_RESET_PROCESS,
+        pid.to_be_bytes().to_vec(),
+    );
+    let bytes = packet.convert_to_bytes();
+    stream.write_all(&bytes).unwrap();
+}
+
+fn reset_all(mut stream: std::os::unix::net::UnixStream) {
+    let packet = Gaiproto::new(
+        gaiproto::MIN_PACKET_SIZE as u32,
+        gaiproto::K_RESET_ALL,
+        Vec::new(),
+    );
+    let bytes = packet.convert_to_bytes();
+    stream.write_all(&bytes).unwrap();
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut path = std::env::temp_dir();
+    path.push(UDS_FILENAME);
+    let stream = std::os::unix::net::UnixStream::connect(&path).unwrap();
+
+    match args.command {
+        Commands::Run { executable, args } => {
+            run(executable, args, stream);
+        }
+        Commands::ResetProcess { pid } => {
+            reset_process(pid, stream);
+        }
+        #[allow(unused)]
+        Commands::ResetAll => {
+            reset_all(stream);
         }
     }
 }
